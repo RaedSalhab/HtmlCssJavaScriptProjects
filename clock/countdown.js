69c4fbdcaca937e4a1524b07a1705f2356d700fa
raed <eng.salhab@gmail.com>
2022-03-01 20:43:42 +0200

    Countdown timer code work

--- clock/countdown.js
+++ clock/countdown.js
@@ -1,66 +1,90 @@
 function addZero(i) {
-    if (i < 10) {i = "0" + i}
-    return i;
+    let j = i.toString(10)
+    if (j.length < 2) {j = "0" + j}
+    return j;
   }
 
 // Countdown Timer Code
-let strSecond = 0;
-let strMinute = 0;
-let strHour = 0;
+let second = 0;
+let minute = 0;
+let hour = 0;
+let timer;
+let countdown = document.getElementById("countdown");
 
 function startTimerFunc() {
-    console.log("satrt")
+    readScreenFunc();
+    timer = setInterval(subSecondFunc, 1000);
 }
   
 function stopTimerFunc() {
-    console.log("stop")
+    clearInterval(timer);
+    countdown.innerHTML = "00:00:00";
 }
 
 function pauseTimerFunc() {
-    console.log("pause")
+    clearInterval(timer);
 }
 
 function addSecondFunc() {
-    strSecond++;
-    print();
-}
-
-function addMinuteFunc() {
-    console.log("+m")
-    strMinute++;
-    print();
-}
-
-function addHourFunc() {
-    strHour++;
-    print();
-}
-
-function subSecondFunc() {
-    if(strSecond > 0) {
-        strSecond--
+    second++;
+    if(second >= 60) {
+        second -= 60;
+        addMinuteFunc();
     }
     print();
 }
 
+function addMinuteFunc() {
+    minute++;
+    if(minute >= 60) {
+        minute -= 60;
+        addHourFunc();
+    }
+    print();
+}
+
+function addHourFunc() {
+    if(hour <= 99) { hour++; }
+    print();
+}
+
+function subSecondFunc() {
+    if(second > 0) {
+        second--
+    } else if (minute >= 1 || hour >= 1) {
+        subMinuteFunc();
+        second = 59;
+    } 
+    print();
+}
+
 function subMinuteFunc() {
-    if(strMinute > 0) {
-        strMinute-- 
+    if(minute > 0) {
+        minute-- 
+    } else if (hour >= 1) {
+        subHourFunc();
+        minute = 59;
     }
     print();
 }
 
 function subHourFunc() {
-    if(strHour > 0) {
-        strHour--
-    }   
+    if(hour > 0) { hour--; } 
     print();
 }  
 
 
 function print() {
-    strSecond = addZero(strSecond);
-    strMinute = addZero(strMinute);
-    strHour = addZero(strHour);
-    document.getElementById("countdown").innerHTML = `${strHour}:${strMinute}:${strSecond}`
+    let stringSecond = addZero(second);
+    let stringMinute = addZero(minute);
+    let stringHour = addZero(hour);
+    countdown.innerHTML = `${stringHour}:${stringMinute}:${stringSecond}`;
+}
+
+function readScreenFunc(){
+    let str = countdown.textContent;
+    let strlist = str.split(":");
+    hour = parseInt(strlist[0]);
+    minute = parseInt(strlist[1]);
+    second = parseInt(strlist[2]);
 }

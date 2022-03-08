cbbfcc57f654a5da9cc2e3e67e9b57bb75345b4b
raed <eng.salhab@gmail.com>
2022-03-08 17:10:10 +0200

    Implement calculation function

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,4 +1,7 @@
 let list = [];
+let operators = ["+", "-", "X", "/"]
+let operator = "";
+let operatorIndex = NaN;
 let output = "0"
 let equation = document.getElementById("equation");
 let result = document.getElementById("result");
@@ -7,24 +10,34 @@ function mainFunc(entry) {
     // Main function
     let input = entry.textContent;
     if (input === "AC") {
-        list = [];
+        rsetFunc();
         result.innerHTML = "0";
-        print(list);
     } else if(input === "DEL") {
         list.pop();
-        print(list);
     } else if(input === "=") {
-        calculation(list);
-    } else if(input === "^" || input == "%" || input == "/" || input == "X" || input == "-" || input == "+"){
+        if(operatorIndex != 0 && operatorIndex != list.length - 1) {
+            calculation();
+        }
+    } else if(operators.includes(input)){
+        if(operators.includes(list[list.length - 1])) {
+            list.pop();
+        } else if(operatorIndex == 0){
+            if(operator == "-"){
+                operator = "";
+                operatorIndex = NaN;
+            } else {
+                list.shift();
+            }
+        } else if(operator !== "" && operatorIndex !== NaN){
+            calculation();
+        }
+        operator = input;
+        operatorIndex = list.length;
         list.push(input);
-        print(list);
-    } else if(input === "(" || input == ")") {
-        list.push(input);
-        print(list);
     } else {
         list.push(input);
-        print(list);
     }
+    print(list);
 }
 
 function print(lst) {
@@ -45,6 +58,36 @@ function print(lst) {
 }
 
 
-function calculation(lst) {
-    result.innerHTML = list;
+function calculation() {
+    let string1 = list.slice(0, operatorIndex);
+    console.log(string1);
+    let string2 = list.slice(operatorIndex + 1, list.length)
+    console.log(string2);
+    let operand1 = parseFloat(string1.join(""));
+    console.log(operand1);
+    let operand2 = parseFloat(string2.join(""));
+    console.log(operand2);
+
+    if(operator === "+") {
+        output = operand1 + operand2;
+    } else if(operator === "-") {
+        output = operand1 - operand2;
+    } else if(operator === "X") {
+        output = operand1 * operand2;
+    } else if(operator === "/") {
+        output = operand1 / operand2;
+    } else {
+        output = 0;
+    }
+    output = Math.round(output * 1000) /1000;
+    result.innerHTML = output;
+    rsetFunc();
+    list.push(output.toString());
+}
+
+function rsetFunc() {
+    list = [];
+    operator = "";
+    operatorIndex = NaN;
+    
 }
